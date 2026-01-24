200828cc669290c3f436be3dad02975845c125f8
sofiaflores18 <hellofloressofia@gmail.com>
2026-01-24 18:04:39 -0500

    doc: import link in README

--- autodoc/doc-generation/dfs_file_traverser.py
+++ autodoc/doc-generation/dfs_file_traverser.py
@@ -146,8 +146,8 @@ def main():
     parser.add_argument("--filepath", type=str, default="")
     parseargs = parser.parse_args()
 
-    current_directory= Path(parseargs.filepath)
-    #current_directory = Path(r"c:\Users\adamm\Documents\PROJECTS\CodingAgent")
+    #current_directory= Path(parseargs.filepath)
+    current_directory = Path(r"C:\Users\hello\Auto-Doc\test_directory")
     depth_traversal(current_directory)              
 
 main()          
